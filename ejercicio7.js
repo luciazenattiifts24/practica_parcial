eff5b3f2ea371557c24df965bcad2173d7563eab
fbifts11 <federico.balbuena@bue.edu.ar>
2025-05-13 02:22:08 -0300

    Subo comentarios en ejercicio7.js. Excelente trabajo!

--- ejercicio7.js
+++ ejercicio7.js
@@ -13,3 +13,36 @@ function decrease(){
 
 btnIncrement.onclick('click').increment()
 btnDecrease.onclick('click').decrease() // aqui probe que si ponia = en vez de . funciona de la misma forma, por que?
+
+
+//No logro comprender a que te referis con "lo habia puesto atras y lo..." no me doy cuenta que es lo que hiciste.
+
+/* 
+Vamos con el otro:
+onclick no es un metodo, es una propiedad, entonces no recibe parámetros, sino que podes asignarle una función, por eso funciona con el =
+El códgio asi como está es erróneo y hace que el comportamiento sea inadecuado, por eso el contador arranca en 1 (no se si te diste cuenta).
+Y hay algo mas, reemplazá tu codigo por esto y fijate el comportamiento de contador en la consola:
+
+btnIncrement.onclick = increment()
+console.log(contador)
+btnDecrease.onclick = decrease()
+console.log(contador)
+
+Cuando carga la pagina vas a ver el contador en 0, pero si mirás la consola vas a ver que primero está en 1 y luego baja a 0.
+Eso es porque al poner btnIncrement.onclick = increment(), con los (), cuando se lee esa línea lo que estás haciendo es INVOCAR a la funcion propiamente, lo que hace que esta se ejecute. Lo mismo con decrease()
+
+En cambio, si pones:
+
+btnIncrement.onclick = increment
+console.log(contador)
+btnDecrease.onclick = decrease
+console.log(contador)
+
+Lo que estás haciendo es pasarle la REFERENCIA de la funcion, y solo se va a ejecutar cuando hagas click en el botón
+
+Entonces la forma correcta para tu cófigo es:
+
+btnIncrement.onclick = increment
+btnDecrease.onclick = decrease
+
+*/
